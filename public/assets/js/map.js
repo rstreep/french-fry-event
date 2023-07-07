d08aac6ca62021524a1c1cefceb5330d0bfff659
Alice Li <aliceli083@gmail.com>
2023-07-07 13:32:29 -0700

    map changes

--- public/assets/js/map.js
+++ public/assets/js/map.js
@@ -1,42 +1,36 @@
 var map = L.map('map', {
     layers: MQ.mapLayer(),
-    center: [39.228537499999995, -123.10585798903601],
+    center: [49.228537499999995, -123.10585798903601],
     zoom: 12,
 });
+var previousRouteLayer; // Declare variable to store the previous route layer
 
 var marker; // Variable to store the marker
-
+var finalLat;
+var finalLong;
 // Click event handler
 function onMapClick(e) {
-    console.log(e.latLng);
-    var lat = e.latlng.lat.toString();
-    var lng = e.latlng.lng.toString();
-
+    console.log(e.latlng);
+    var lat = e.latlng.lat;
+    var lng = e.latlng.lng;
+    
     // Remove previous marker if it exists
     if (marker) {
-        map.removeLayer(marker);
+    map.removeLayer(marker);
     }
-
+    
     // Add a new marker at the clicked location
     marker = L.marker([lat, lng]).addTo(map);
-
+    
     // Retrieve the coordinates
-    var markerLat = marker.getLatLng().lat;
-    var markerLng = marker.getLatLng().lng;
-
-
-    // coordinates 
-
+    var markerLat = lat;
+    var markerLng = lng;
+    
+    // coordinates
     console.log('Marker Coordinates:', markerLat, markerLng);
-    postMapData("test", markerLat, markerLng);
-
-    // var customIcon = L.divIcon({
-    // className: 'custom-marker',
-    // html: `<div class="marker-label">${text}</div>`
-
-    // });
-    // marker.setIcon(customIcon)
-}
+    finalLat = markerLat;
+    finalLong = markerLng;
+    }
 
 // Add the click event listener to the map
 map.on('click', onMapClick);
@@ -53,98 +47,132 @@ async function postMapData(text, lat, lng) {
     console.log(response);
 };
 function displayModal(e) {
-    if (e) {
-        document.querySelector("#lat").value = e.latlng.lat;
-        document.querySelector("#lng").value = e.latlng.lng;
-    }
+    // if (e) {
+    //     document.querySelector("#lat").value = e.latlng.lat;
+    //     document.querySelector("#lng").value = e.latlng.lng;
+    // }
     var modal = document.querySelector("#modal1");
     if (modal.dataset.state === "show") {
         modal.dataset.state = "hidden";
-        // modal.textContent = "";
+        modal.style.display = "none"; // Set display to "none" to hide the modal
+    } else {
+    modal.dataset.state = "show";
+    modal.style.display = ""; // Reset display to its default value to show the modal
     }
-    else {
-        modal.dataset.state = "show";
-        // modal.textContent = modal.dataset.num;
-    }
+    
 }
 function captureText(e) {
-    e.preventDefault()
+    e.preventDefault();
     var iconNameInput = document.querySelector("#icon-name-input");
     var text = iconNameInput.value;
-    var lat = document.querySelector("#lat").value
-    var lng = document.querySelector("#lng").value
-    displayModal()
-    onMapClick(text, lat, lng)
-    postMapData(text, lat, lng)
+    
+
+    displayModal();
+
+    // Retrieve the finalLat and finalLong from onMapClick function
+    var lat = finalLat.toString();
+    var lng = finalLong.toString();
+
+    postMapData(text, lat, lng);
 }
+
+function removeMarker(e) {
+    e.preventDefault()
+    if (marker) {
+        map.removeLayer(marker);
+        marker = null; // Reset the marker variable
+        }
+}
+
+
 // Add the click event listener to the map
 // map.addEventListener('click', onMapClick);
 map.addEventListener('click', displayModal);
-var iconNameForm = document.querySelector("#icon-name");
-iconNameForm.addEventListener('submit', captureText);
-function runDirections(start, end) {
-    var dir = MQ.routing.directions();
-    dir.route({
-        locations: [
-            start,
-            end
-        ]
-    });
 
-    CustomRouteLayer = MQ.Routing.RouteLayer.extend({
-        createMarker: (location) => {
-            var lat = location.latLng.lat;
-            var lng = location.latLng.lng;
-            var marker = L.marker([lat, lng]).addTo(map);
-            return marker;
-        },
-    });
-
-    var routeLayer = new CustomRouteLayer({
-        directions: dir,
-        fitBounds: true
-    });
-
-    routeLayer.on('draw:created', function (e) {
-        layer = e.layer;
-        var lat = layer.getLatLng().lat;
-        var lng = layer.getLatLng().lng;
-
-        if (e.layerType === 'marker') {
-            var marker = L.marker([lat, lng]).addTo(map);
-            return marker;
-        }
-    });
-
-    map.addLayer(routeLayer);
-}
-
-//runs when form is submitted
-function submitForm(event) {
-    event.preventDefault();
-
-    //delete current map layer (so when you type in a new location the old route doesn't stay on the map)
-    if (layer) {
-        map.removeLayer(layer);
+function cancelModal(){
+    var modal = document.querySelector("#modal1");
+    if(modal.dataset.state === "show"){
+    modal.dataset.state = "hidden";
+    // modal.textContent = "";
+    }
+    else{
+    modal.dataset.state = "show";
+    // modal.textContent = modal.dataset.num;
+    }
     }
 
-    // get form data
+    var iconNameForm = document.querySelector("#icon-name");
+    iconNameForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    captureText(event);
+    });
+
+
+
+
+document.getElementById("cancel-button").addEventListener("click", function(e) {
+    e.preventDefault(); // Prevent form submission
+ 
+    cancelModal();
+    removeMarker();
+    modal.style.display = "none";
+  
+    if (marker) {
+      map.removeLayer(marker);
+    }
+    return false;
+  });
+
+  function runDirections(start, end) {
+    var dir = MQ.routing.directions();
+    dir.route({
+      locations: [start, end]
+    });
+  
+    CustomRouteLayer = MQ.Routing.RouteLayer.extend({
+      createMarker: (location) => {
+        var lat = location.latLng.lat;
+        var lng = location.latLng.lng;
+        var marker = L.marker([lat, lng]).addTo(map);
+        return marker;
+      },
+    });
+  
+    // Remove previous route layer if it exists
+    if (previousRouteLayer) {
+      map.removeLayer(previousRouteLayer);
+    }
+  
+    var routeLayer = new CustomRouteLayer({
+      directions: dir,
+      fitBounds: true
+    });
+  
+    routeLayer.on('draw:created', function (e) {
+      // Your code for handling the created layer
+    });
+  
+    map.addLayer(routeLayer);
+  
+    // Store the current route layer as the previous one
+    previousRouteLayer = routeLayer;
+  }
+  
+  function submitForm(event) {
+    event.preventDefault();
+  
+    // Delete current map layer (so the old route doesn't stay on the map)
+    if (previousRouteLayer) {
+      map.removeLayer(previousRouteLayer);
+    }
+  
+    // Get form data
     var start = document.getElementById("start").value;
     var end = document.getElementById("destination").value;
-
-    //run directions function
+  
+    // Run directions function
     runDirections(start, end);
-}
-
-const form = document.getElementById('form');
-//call the submitForm function when submit
-form.addEventListener('submit', submitForm);
-
-    // setTimeout(function () {
-    // var urlParams = new URLSearchParams(window.location.search);
-    // var start = urlParams.get("start");
-    // var destination = urlParams.get("destination");
-    // $("#start").val(start);
-    // $("#destination").val(destination);
-    // $("#directions").click(); //how to add it in the separate window? - window.open(url, '_blank');
-    // }, 500);
+  }
+  
+  const form = document.getElementById('form');
+  form.addEventListener('submit', submitForm);
