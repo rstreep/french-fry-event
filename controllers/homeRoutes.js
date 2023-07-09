69f6e6db707b0a45f0ce5bd8e1bf80e94e49270c
mayamouse <cooperpontea@gmail.com>
2023-07-09 17:36:18 -0400

    draft routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -7,6 +7,8 @@
 const router = require('express').Router();
 const { ff_event_guest_map, ff_event, ff_lookup, ff_menu, User} = require('../models');
 const withAuth = require('../utils/auth');
+const sequelize = require('../../config/connection');
+const Op = sequelize.Op
 
 router.get('/', async (req, res) => {
     // res.send('homeRoutes');
@@ -29,8 +31,134 @@ router.get('/', async (req, res) => {
 
 router.get('/map', (req, res) => {
   res.render('map');
-})
+});
 
+// Route: Get Event Card 1
+router.get('/event', async (req, res) => {
+  try {
+  const event1 = await Event.findOne({ 
+    limit: 3, 
+    where: { key },
+    order: [ [ 'event_date', 'DESC' ]],
+  });
+      if (!event1) {
+          return res.status(404).json({ error: 'Event not found.' });
+      }
+      res.status(200).json(event1);
+  } catch (error) {
+      res.status(500).json({ error: 'An error occurred while fetching the event.' });
+  }
+});
+  
+// Route: Get Event Card 2
+router.get('/event', async (req, res) => {
+  try {
+  const event2 = await Event.findOne({ 
+    limit: 2,
+    where: { key },
+    order: [ [ 'event_date', 'ASC' ]],
+  });
+      if (!event2) {
+          return res.status(404).json({ error: 'Event not found.' });
+      }
+      res.status(200).json(event2);
+  } catch (error) {
+      res.status(500).json({ error: 'An error occurred while fetching the event.' });
+  }
+});
+
+// Route: Get Event Card 3
+router.get('/event', async (req, res) => {
+  try {
+  const event3 = await Event.findOne({ 
+    limit: 2,
+    where: { key },
+    order: [ [ 'event_date', 'ASC' ]],
+  });
+      if (!event3) {
+          return res.status(404).json({ error: 'Event not found.' });
+      }
+      res.status(200).json(event3);
+  } catch (error) {
+      res.status(500).json({ error: 'An error occurred while fetching the event.' });
+  }
+});
+
+
+// Route: Get Upcoming Events
+router.get('/events', async (req, res) => {
+  try {
+  const upcoming = await Event.findall({ 
+    limit: 5, 
+    where: { key },
+    order: [ [ 'event_date', 'DESC' ]],
+  });
+      if (!upcoming) {
+          return res.status(404).json({ error: 'Event not found.' });
+      }
+      res.status(200).json(upcoming);
+  } catch (error) {
+      res.status(500).json({ error: 'An error occurred while fetching the event.' });
+  }
+});
+// Route: Get Events I'm Hosting
+router.get('/events', async (req, res) => {
+  try {
+  const now = moment()
+  const hosting = await Event.findall({ 
+    limit: 5, 
+    where: { event_date: {
+      [Op.lt]: now.endOf('day').toString()
+    },
+    host_user_id: req.params.host_id
+   },
+    order: [ [ 'event_date', 'DESC' ]],
+  });
+      if (!hosting) {
+          return res.status(404).json({ error: 'Event not found.' });
+      }
+      res.status(200).json(hosting);
+  } catch (error) {
+      res.status(500).json({ error: 'An error occurred while fetching the event.' });
+  }
+});
+// Route: Get Past Events
+router.get('/events', async (req, res) => {
+  try {
+  const now = moment()
+  const upcoming = await Event.findall({ 
+    limit: 5, 
+    where: { event_date: {
+      [Op.lt]: now.endOf('day').toString()
+    }
+   },
+    order: [ [ 'event_date', 'DESC' ]],
+  });
+      if (!upcoming) {
+          return res.status(404).json({ error: 'Event not found.' });
+      }
+      res.status(200).json(upcoming);
+  } catch (error) {
+      res.status(500).json({ error: 'An error occurred while fetching the event.' });
+  }
+});
+
+//  * Route: GET /events
+//  * Description: Renders the events page with a list of events.
+//  *              Only accessible to authenticated users.
+//  */ router.get('/events', withAuth, async (req, res) => {
+//   try {
+//     // Fetch the list of events from the database
+//     const events = await ff_event.findAll();
+
+//     // Render the events page with the list of events
+//     res.render('events', { events });
+//   } catch (err) {
+//     // Handle errors
+//     console.error(err);
+//     res.status(500).json({ error: 'Failed to retrieve events' });
+//   }
+// });
 
 
 ///////////////////////// - Example of possible routers - need models and seed implementation to complete/////////////////////////////////
