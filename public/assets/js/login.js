bc2fb3b5c4b950a32d17a54bceeca857a6ee6ba3
rstreep <127055958+rstreep@users.noreply.github.com>
2023-07-09 17:46:26 -0400

    bug fixing

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -18,7 +18,7 @@ const loginFormHandler = async (event) => {
     // Collect values from the login form
     const email = document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
-  
+    
     if (email && password) {
       // Send a POST request to the '/api/users/login' endpoint
       const response = await fetch('/api/users/login', {
@@ -26,7 +26,6 @@ const loginFormHandler = async (event) => {
         body: JSON.stringify({ email, password }),
         headers: { 'Content-Type': 'application/json' },
       });
-  
       if (response.ok) {
         // If the response is successful, redirect the browser to the profile page
         document.location.replace('/');
